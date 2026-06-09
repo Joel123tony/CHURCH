407ce318f118ddd3a412c8cb1fb455983508b58d
Joel123tony <1234eruk1637@email.com>
2026-06-09 15:50:01 +0530

    fix MediaCard component location

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
-import galleryRoutes from "./routes/gallery.routes.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
 
 app.use("/api/gallery", galleryRoutes);
 
