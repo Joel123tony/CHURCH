5e1f359c2f3b06bdf9125d1583bcb8da8ea889ae
Joel123tony <1234eruk1637@email.com>
2026-06-09 10:59:34 +0530

    fix: connect mongodb in netlify function

--- server/app.js
+++ server/app.js
@@ -1,24 +1,23 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
 
 import authRoutes from "./routes/authRoutes.js";
 import adminRoutes from "./routes/adminAuth.routes.js";
 import pastorRoutes from "./routes/pastor.routes.js";
 
+dotenv.config();
+connectDB();
+
 const app = express();
 
-/* =========================
-   MIDDLEWARE ORDER (VERY IMPORTANT)
-========================= */
 app.use(cors());
-app.use(express.json());   // 🔥 MUST be BEFORE routes
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-/* =========================
-   ROUTES
-========================= */
 app.use("/api/auth", authRoutes);
 app.use("/api/admin", adminRoutes);
 app.use("/api/pastors", pastorRoutes);
