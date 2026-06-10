51b49e9e1ce36effdacc98ff0fac3ce775907244
Joel123tony <1234eruk1637@email.com>
2026-06-10 19:51:39 +0530

    login page

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,12 +1,9 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://church-rp0n.onrender.com/api",
 });
 
-/* =========================
-   ATTACH TOKEN AUTOMATICALLY
-========================= */
 API.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("token");
