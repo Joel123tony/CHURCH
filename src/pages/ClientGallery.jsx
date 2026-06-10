b24b99d060d4b57ddc9e1093d7b294dc7384b20e
Joel123tony <1234eruk1637@email.com>
2026-06-10 22:47:36 +0530

    gallery

--- src/pages/ClientGallery.jsx
+++ src/pages/ClientGallery.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import API from "../../api/axios";
+import API from "../api/axios";
 
 export default function ClientGallery() {
   const [media, setMedia] = useState([]);
