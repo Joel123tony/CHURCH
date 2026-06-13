f3345023bc61ee1c98e73ab9f8e282413c4692bf
Joel123tony <1234eruk1637@email.com>
2026-06-13 19:19:39 +0530

    Fix CORS PATCH support

--- server/server.js
+++ server/server.js
@@ -35,12 +35,11 @@ app.use(
   })
 );
 
-app.options("*", cors());
-
 /* =========================
    MIDDLEWARE
 ========================= */
 app.use(express.json({ limit: "50mb" }));
+
 app.use(
   express.urlencoded({
     extended: true,
